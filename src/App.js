63ebd361262fbd299ae27790c7c3386e6db2932b
new-era-media <53826571+new-era-media@users.noreply.github.com>
2020-08-27 17:32:55 +0300

    create component v2

--- src/App.js
+++ src/App.js
@@ -3,25 +3,123 @@ import "./App.scss";
 import Card from "./Card/Card";
 
 class App extends Component {
+  test = (e, el) => {
+    if (!e.target.closest(".disable")) {
+      let shoppingСart = this.state.shoppingСart;
+      if (this.state.shoppingСart.indexOf(el) === -1) {
+        let shoppingСart = this.state.shoppingСart;
+        shoppingСart.push(el);
+      } else {
+        shoppingСart = shoppingСart.filter((word) => word !== el);
+      }
+      this.setState({
+        shoppingСart,
+      });
+    }
+  };
+
   state = {
     cards: [
       {
-        filling: "фуагра",
-        portions: "10 порций",
-        gift: "Мышь в подарок",
-        volume: "0.5",
+        dataname: "Fuagra",
+        filling: "фуа-гра",
+        portions: (
+          <span>
+            <strong>10</strong> порций
+          </span>
+        ),
+        gift: "мышь в подарок",
+        volume: "0,5",
+        text: (
+          <span>
+            Чего сидишь? Порадуй котэ,
+            <span className="Pay" onClick={this.test.bind(this, "Fuagra")}>
+              {" "}
+              купи.
+            </span>
+          </span>
+        ),
+        enable: true,
+        elementColor: "blue",
+      },
+      {
+        dataname: "Fish",
+        filling: "рыбой",
+        portions: (
+          <span>
+            <strong>40</strong> порций
+          </span>
+        ),
+        gift: (
+          <span>
+            <strong>2 </strong>мыши в подарок
+          </span>
+        ),
+        volume: "2",
+        text: "Головы щучьи с чесноком, да свежайшая сёмгушка.",
+        enable: true,
+        elementColor: "pink",
+      },
+      {
+        dataname: "Сhicken",
+        filling: "курой",
+        portions: (
+          <span>
+            <strong>100</strong> порций
+          </span>
+        ),
+        gift: (
+          <span>
+            <strong>5 </strong>мыши в подарок
+          </span>
+        ),
+        volume: "5",
         text: "Чего сидишь? Порадуй котэ, купи.",
+        feedback: "Заказчик доволен",
+        enable: false,
       },
     ],
+    shoppingСart: [],
   };
 
   render() {
     return (
       <div className="App">
+        <p
+          style={{
+            color: "white",
+            position: "absolute",
+            top: "10px",
+            left: "10px",
+            zIndex: "99",
+          }}
+        >
+          {this.state.shoppingСart.join(" ")}
+        </p>
         <h2 className="App-title">Ты сегодня покормил кота?</h2>
-        <div className="frame-cards">
+        <div className="App-frame-cards">
           {this.state.cards.map((item, index) => {
-            return <Card text={item.text} key={index} />;
+            return (
+              <Card
+                // addHendler={this.test.bind(
+                //   this,
+                //   this.state.cards[index].dataname
+                // )}
+                addHendler={(e) => {
+                  this.test(e, this.state.cards[index].dataname);
+                }}
+                dataname={item.dataname}
+                filling={item.filling}
+                portions={item.portions}
+                gift={item.gift}
+                volume={item.volume}
+                text={item.text}
+                feedback={item.feedback}
+                enable={item.enable}
+                elementColor={item.elementColor}
+                key={index}
+              />
+            );
           })}
         </div>
       </div>
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,47 @@
 import React from "react";
 import "./Card.scss";
 
-export default (props) => (
-  <div className="card-container">
-    <div className="card-container__card">
+const Card = (props) => {
+  const disabled = props.enable ? "card-container" : "card-container disable";
+
+  const description = props.enable
+    ? props.text
+    : `Печалька, с ${props.filling} закончилось`;
+
+  const style = {};
+  props.enable ? (style.color = "white") : (style.color = "yellow");
+
+  const elColor =
+    props.elementColor === "blue"
+      ? { backgroundImage: `url(${require("../assets/image/bg_card-1.png")})` }
+      : { backgroundImage: `url(${require("../assets/image/bg_card-2.png")})` };
+
+  const bgColor =
+    props.elementColor === "blue"
+      ? { backgroundColor: "#1898D9" }
+      : { backgroundColor: "#DB1668" };
+
+  return (
+    <div className={disabled} data-name={props.dataname}>
+      <div className="card-container__card card" onClick={props.addHendler}  style={elColor}>
+        <p className="card-container__card_preTitle">
+          Сказачное заморское яство
+        </p>
+        <h2 className="card-container__card_title">Нямушка</h2>
+        <p className="card-container__card_filling">с {props.filling}</p>
+        <p className="card-container__card_portion">{props.portions}</p>
+        <p className="card-container__card_gift">{props.gift}</p>
+        <p className="card-container__card_feedback">{props.feedback}</p>
+        <div className="card-container__card_circle" style={bgColor}>
+          <p className="card_circle-number">{props.volume}</p>
+          <p className="card_circle-unit">кг</p>
+        </div>
       </div>
-    <p className="card-container__text">
-        {props.text}
-    </p>
-  </div>
-);
+      <p className="card-container__text" style={style}>
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default Card;
