d6609f7493d6301199ab71615ea8f0a4fee0fa58
new-era-media <53826571+new-era-media@users.noreply.github.com>
2020-08-27 17:39:07 +0300

    Updates

---
+++ precache-manifest.cf258d530f4f0ff9909f0fdf8a9b68eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46a90ee709c8ff55ab047befd0a9db0",
+    "url": "/Did-you-feed-the-cat-/index.html"
+  },
+  {
+    "revision": "c61c3843e99fac514c57",
+    "url": "/Did-you-feed-the-cat-/static/css/main.0e44f8c0.chunk.css"
+  },
+  {
+    "revision": "d88f4af583e004869555",
+    "url": "/Did-you-feed-the-cat-/static/js/2.0ed1677c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Did-you-feed-the-cat-/static/js/2.0ed1677c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61c3843e99fac514c57",
+    "url": "/Did-you-feed-the-cat-/static/js/main.698b1bec.chunk.js"
+  },
+  {
+    "revision": "225411038c80b00ebeff",
+    "url": "/Did-you-feed-the-cat-/static/js/runtime-main.fafcdf93.js"
+  },
+  {
+    "revision": "48c4ef39806ef76eaeced814b8a5705c",
+    "url": "/Did-you-feed-the-cat-/static/media/9412.48c4ef39.ttf"
+  },
+  {
+    "revision": "eb9b32a104dff924c8379fff8b6ca053",
+    "url": "/Did-you-feed-the-cat-/static/media/Cat.eb9b32a1.png"
+  },
+  {
+    "revision": "d2deaf81a223f839cbc9108a59e333a9",
+    "url": "/Did-you-feed-the-cat-/static/media/bg.d2deaf81.jpg"
+  }
+]);
